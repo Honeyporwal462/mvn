e490a6fc335e00307a8fdf3d485b54ae06e2ecd2
Honeyporwal462 <Honeyporwal462@gmail.com>
2020-07-13 15:56:38 +0530

    new 1

---
+++ src/main/java/com/hp/First_Program.java
@@ -0,0 +1,8 @@
+package com.hp;
+
+public class First_Program {
+	public static void main(String args[]) {
+		System.out.println("HEllo World");
+	}
+
+}
---
+++ src/test/java/com/hp/First_ProgramTest.java
@@ -0,0 +1,14 @@
+package com.hp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class First_ProgramTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
